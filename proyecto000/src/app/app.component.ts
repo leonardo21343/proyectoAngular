d304c875ef660e04839ac4fd9bd46a56ed82ad60
leonardo <jerifloreszenx@gmail.com>
2024-03-18 14:00:32 -0500

    agregando archivos

--- proyecto000/src/app/app.component.ts
+++ proyecto000/src/app/app.component.ts
@@ -9,5 +9,24 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'proyecto000';
+  nombre = 'Rodriguez Pablo';
+  edad = 40;
+  sueldos = [1700, 1600, 1900];
+  activo = true;
+
+  esActivo() {
+    if (this.activo === true) {
+      return "Trabajador Activo";
+    } else {
+      return "Trabajador Inactivo";
+    }
+  }
+
+  mostrasSueldo() {
+    let suma = 0;
+    for (let i = 0; i < this.sueldos.length; i++) {
+      suma += this.sueldos[i];
+    }
+    return suma;
+  }
 }
